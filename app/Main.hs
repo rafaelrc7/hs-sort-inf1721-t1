f1506884b4cd3e894de5fd4a422e11842b900e16
rafaelrc7 <rafaelrc7@gmail.com>
2022-04-10 02:54:36 -0300

    Starting Main implementation

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,22 @@
 module Main where
 
+import System.CPUTime
+import qualified InsertionSort as I
+import qualified MergeSort     as M
+import qualified QuickSort     as Q
+
+data BenchmarkOps = BenchmarkOps
+  { checkResult :: Bool
+  , printResult :: Bool
+  }
+
 main :: IO ()
 main = putStrLn "Hello, Haskell!"
+
+benchmark :: (Ord a, Fractional b) => [a] -> ([a] -> [a]) -> IO b
+benchmark xs sort = do
+  start <- getCPUTime
+  let xs' = sort xs
+  end <- getCPUTime
+  return $ (fromIntegral (end - start)) / (10^12)
+
