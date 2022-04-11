687ed78047e152f0b94a3a32001a59d949cc491a
rafaelrc7 <rafaelrc7@gmail.com>
2022-04-10 22:01:12 -0300

    Fixed benchmark function and implemented file reading

--- app/Main.hs
+++ app/Main.hs
@@ -17,7 +17,32 @@ data BenchmarkOps = BenchmarkOps
   }
 
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = executeInp >> putStrLn "Done."
+
+executeInp :: IO [[[Int]]]
+executeInp = do
+  inp <- getContents
+  let (nTests:ls) = lines inp
+  let tests = parseTests (read nTests) ls
+  let results = executeTest <$> tests
+  sequence results
+
+parseTest :: [String] -> (String, [Int], [String])
+parseTest (tName:tEls:ls) = (tName, ns, ls')
+  where parseTest' :: Int -> [String] -> ([Int], [String])
+        parseTest' 0 ls = ([], ls)
+        parseTest' n (l:ls) = (read l : ns, ls')
+          where (ns, ls') = parseTest' (n-1) ls
+        (ns, ls') = parseTest' (read tEls) ls
+
+parseTests :: Int -> [String] -> [(String, [Int])]
+parseTests 0 _  = []
+parseTests n ls = (tLabel, tNums) : parseTests (n-1) ls'
+  where (tLabel, tNums, ls') = parseTest ls
+
+executeTest :: Ord a => (String, [a]) -> IO [[a]]
+executeTest (label, xs) = putStrLn ("\nExecuting test " ++ label)
+                         >> benchmarkAll xs
 
 benchmarkAll :: Ord a => [a] -> IO [[a]]
 benchmarkAll xs = do
@@ -29,10 +54,10 @@ benchmarkAll xs = do
 benchmark :: Ord a => ([a] -> [a]) -> [a] -> Maybe String -> IO [a]
 benchmark sort xs msg = do
   start <- getTime Monotonic
-  let res = evaluate $ sort xs
+  res <- evaluate $ sort xs
   end <- getTime Monotonic
   case msg of Just msg -> putStr (msg ++ " >> ")
                 >> fprint (timeSpecs % "\n") start end
               Nothing -> fprint (timeSpecs % "\n") start end
-  res
+  return res
 
